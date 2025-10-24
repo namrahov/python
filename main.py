3ab1ed1ba6cd32147500163462a9942a90a6493d
Nurlan Amrahov <nurlan.amrahov@ailab.az>
2025-10-24 12:44:08 +0400

    Add saveAll

--- main.py
+++ main.py
@@ -4,15 +4,37 @@
 # if __name__ == "__main__":
 #     print(greet("World"))
 import os
+import openpyxl
 import shutil
 
-from service.UserService import save_user
+from service.UserService import save_all
 from db.Database import Base, engine
 
 Base.metadata.create_all(bind=engine)
 
 if __name__ == "__main__":
-    save_user("Tarkhan", "tarkhan@example.com")
+
+    os.chdir('C:\\Users\\AZINTELECOM\\OneDrive\\İş masası')
+    workbook = openpyxl.load_workbook('siyahi.xlsx', read_only = True)
+    users = []
+
+    for sheet in workbook.sheetnames:
+        ws = workbook[sheet]
+
+    for row in ws.iter_rows(values_only=True):
+        print(row)
+        if row is not None:
+            name = row[0]
+            email = row[1]
+            users.append({"name": name, "email": email})
+
+    if users:
+        save_all(users)
+    else:
+        print("⚠️ No valid rows found to save.")
+
+
+   # save_user("Tarkhan", "tarkhan@example.com")
    #shutil.copy('C:\\Users\\AZINTELECOM\\IdeaProjects\\spam.txt', 'C:\\Users\\AZINTELECOM')
  # for folderName, subfolders, filenames in os.walk('C:\\Users\\AZINTELECOM\\IdeaProjects'):
  #       print(filenames)
--- service/UserService.py
+++ service/UserService.py
@@ -2,16 +2,38 @@ from models.User import User
 from db.Database import SessionLocal
 
 
-def save_user(name, email):
+def save_all(users):
+    """
+    users: list of dicts or tuples, e.g.
+    [
+        {"name": "Alice", "email": "alice@example.com"},
+        {"name": "Bob", "email": "bob@example.com"}
+    ]
+    """
     session = SessionLocal()
     try:
-        user = User(name=name, email=email)
-        session.add(user)
+        user_objects = [User(name=u["name"], email=u["email"]) for u in users]
+        session.add_all(user_objects)
         session.commit()
-        session.refresh(user)
-        print(f"✅ Saved user: {user}")
+        print(f"✅ Saved {len(user_objects)} users.")
     except Exception as e:
         session.rollback()
-        print(f"❌ Error: {e}")
+        print(f"❌ Error saving users: {e}")
     finally:
         session.close()
+
+
+#
+# def save_user(name, email):
+#     session = SessionLocal()
+#     try:
+#         user = User(name=name, email=email)
+#         session.add(user)
+#         session.commit()
+#         session.refresh(user)
+#         print(f"✅ Saved user: {user}")
+#     except Exception as e:
+#         session.rollback()
+#         print(f"❌ Error: {e}")
+#     finally:
+#         session.close()
